3e3d6b0b44e4873ea8f2e044ea7ce3d18111b663
yjj8353 <yjj8353@gmail.com>
2025-04-15 11:17:23 +0900

    기초 커맨드 생성

--- commands/ship.js
+++ commands/ship.js
@@ -28,7 +28,38 @@ module.exports = {
             .setDescription("어떤 어선인지에 대한 설명을 정합니다.")
             .setRequired(false)
         )
-  ),
+      )
+    
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("승선")
+        .setDescription("지정한 어선에 승선합니다.")
+        .addStringOption((option) =>
+          option
+            .setName("선명")
+            .setDescription("승선할 어선을 선택합니다.")
+            .setRequired(true),
+        )
+      )
+
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("하선")
+        .setDescription("지정한 어선에서 하선합니다.")
+        .addStringOption((option) =>
+          option
+            .setName("선명")
+            .setDescription("하선할 어선을 선택합니다.")
+            .setRequired(true),
+        )
+      )
+    
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("여담")
+        .setDescription("이 봇에 대한 여담을 알려드립니다.")
+      )
+    ,
 	async execute(interaction) {
 
     // /어선 생성
@@ -50,6 +81,46 @@ module.exports = {
         footer: {
           text: "어선 생성 완료!",
         },
+      };
+      
+      await interaction.reply({ embeds: [ shipEmbed ] });
+    }
+
+    if (interaction.options.getSubcommand() === "승선") {
+      const shipName = interaction.options.getString("선명");
+      const shipEmbed = {
+        color: 0x0099ff,
+        title: shipName,
+        description: "승선 완료!",
+        timestamp: new Date(),
+        footer: {
+          text: "어선 승선 완료!",
+        },
+      };
+      
+      await interaction.reply({ embeds: [ shipEmbed ] });
+    }
+
+    if (interaction.options.getSubcommand() === "하선") {
+      const shipName = interaction.options.getString("선명");
+      const shipEmbed = {
+        color: 0x0099ff,
+        title: shipName,
+        description: "하선 완료!",
+        timestamp: new Date(),
+        footer: {
+          text: "어선 하선 완료!",
+        },
+      };
+      
+      await interaction.reply({ embeds: [ shipEmbed ] });
+    }
+
+    if (interaction.options.getSubcommand() === "여담") {
+      const shipEmbed = {
+        color: 0x0099ff,
+        title: "어선 여담",
+        description: "이 봇에 사용된 그림은 사실 어선이 아닌 상선의 그림입니다. 그리고 아주 유명한 유령선이죠.\n꺄아아아악!"
       };
       
       await interaction.reply({ embeds: [ shipEmbed ] });
