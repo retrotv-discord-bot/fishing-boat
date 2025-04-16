f258e36ff684bf542823ef0bdd9ad806a0c1d96e
yjj8353 <yjj8353@gmail.com>
2025-04-16 23:54:32 +0900

    리팩터링

--- services/ship/index.js
+++ services/ship/index.js
@@ -19,35 +19,8 @@ module.exports = {
       canMidParticipation = interaction.options.getBoolean("중참가능여부");
     }
 
-    if (alarmTime) {
-      alarmTime = alarmTime.trim();
-      alarmTime = alarmTime.replace(":", "");
-      alarmTime = alarmTime.replace(" ", "");
-
-      if (alarmTime.length !== 4 && alarmTime.length !== 3) {
-        return interaction.reply({ content: "출항시간은 24시간 체계의 hhmm 혹은 hh:mm 형식으로 입력해주세요.\n예시: 18:30, 06:00, 9:00, 1000, 2030, 800", ephemeral: true });
-      }
-
-      if (alarmTime.length === 3) {
-        alarmTime = "0" + alarmTime;
-      }
-
-      const hh = alarmTime.substring(0, 2);
-      const mm = alarmTime.substring(2, 4);
-      if (hh < 0 || hh > 23 || mm < 0 || mm > 59) {
-        return interaction.reply({ content: "출항시간이 정상적인 시간의 범주가 아닙니다.", ephemeral: true });
-      }
-
-      const currentTime = new Date();
-      const currentHours = currentTime.getHours();
-      const currentMinutes = currentTime.getMinutes();
-      const currentAlarmTime = parseInt(currentHours.toString().padStart(2, '0') + currentMinutes.toString().padStart(2, '0'), 10);
-      const inputAlarmTime = parseInt(alarmTime, 10);
-
-      if (inputAlarmTime <= currentAlarmTime) {
-        return interaction.reply({ content: "출항시간은 현재 시간보다 미래로 설정해야 합니다.", ephemeral: true });
-      }
-    }
+    // 시간에 대한 유효성 검증
+    this.__checkTime(alarmTime);
 
     const clientId = interaction.user.id;
     const clientName = interaction.user.username;
@@ -265,5 +238,37 @@ module.exports = {
 
     channel.send(userMentions);
     return interaction.reply({ content: "어선에 탑승한 인원들에게 알림을 보냈습니다.", ephemeral: true });
+  },
+
+  __checkTime: (alarmTime) => {
+    if (alarmTime) {
+      alarmTime = alarmTime.trim();
+      alarmTime = alarmTime.replace(":", "");
+      alarmTime = alarmTime.replace(" ", "");
+
+      if (alarmTime.length !== 4 && alarmTime.length !== 3) {
+        return interaction.reply({ content: "출항시간은 24시간 체계의 hhmm 혹은 hh:mm 형식으로 입력해주세요.\n예시: 18:30, 06:00, 9:00, 1000, 2030, 800", ephemeral: true });
+      }
+
+      if (alarmTime.length === 3) {
+        alarmTime = "0" + alarmTime;
+      }
+
+      const hh = alarmTime.substring(0, 2);
+      const mm = alarmTime.substring(2, 4);
+      if (hh < 0 || hh > 23 || mm < 0 || mm > 59) {
+        return interaction.reply({ content: "출항시간이 정상적인 시간의 범주가 아닙니다.", ephemeral: true });
+      }
+
+      const currentTime = new Date();
+      const currentHours = currentTime.getHours();
+      const currentMinutes = currentTime.getMinutes();
+      const currentAlarmTime = parseInt(currentHours.toString().padStart(2, '0') + currentMinutes.toString().padStart(2, '0'), 10);
+      const inputAlarmTime = parseInt(alarmTime, 10);
+
+      if (inputAlarmTime <= currentAlarmTime) {
+        return interaction.reply({ content: "출항시간은 현재 시간보다 미래로 설정해야 합니다.", ephemeral: true });
+      }
+    }
   }
 }
