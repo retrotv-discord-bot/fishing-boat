9c514e3e730e39c5ae9b6f9b979a67c4a2837871
yjj8353 <yjj8353@gmail.com>
2026-03-01 02:09:22 +0900

    로깅레벨 변경

--- src/config/logtape.ts
+++ src/config/logtape.ts
@@ -2,7 +2,13 @@ import { configure, getConsoleSink, getLogger, type Logger } from "@logtape/logt
 
 configure({
     sinks: { console: getConsoleSink() },
-    loggers: [{ category: ["bot"], lowestLevel: "debug", sinks: ["console"] }],
+    loggers: [
+        // 애플리케이션 로그
+        { category: ["bot"], lowestLevel: "debug", sinks: ["console"] },
+
+        // 이 카테고리만 info 메시지 차단
+        { category: ["logtape", "meta"], lowestLevel: "warning", sinks: ["console"] },
+    ],
 })
     .then(() => {
         console.log("Configuration complete");
